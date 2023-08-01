0ae756bfbb6aa98c4f2c96ef98f5a96d7e133512
sunfirehawk2017 <48810907+sunfirehawk2017@users.noreply.github.com>
2023-08-01 08:40:20 -0500

    Create TwoSum.cs

---
+++ C#/Two Sum/TwoSum.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+
+class TwoSum
+{
+    public static Tuple<int, int> FindTwoSum(IList<int> list, int sum)
+    {
+        HashSet<int> hs = new HashSet<int>();
+        for (int i = 0; i < list.Count; i++)
+        {
+            var needed = sum - list[i];
+            if (hs.Contains(needed))
+            {
+                return Tuple.Create(list.IndexOf(needed), i);
+            }
+            hs.Add(list[i]);                
+        }
+        return null;
+    }
+
+    public static void Main(string[] args)
+    {
+        Tuple<int, int> indices = FindTwoSum(new List<int>() { 3, 1, 5, 7, 5, 9 }, 10);
+        if(indices != null) 
+        {
+            Console.WriteLine(indices.Item1 + " " + indices.Item2);
+        }
+    }
+}
